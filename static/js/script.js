eb5e92cd7ba1164884dc24e9187eef065f8fad7a
Georgina Kidger <georgina.kidger@hotmail.com>
2025-01-24 01:30:18 +0000

    added modal code to categories and tasks

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.modal').modal();
     $('select').formSelect();
     $('.datepicker').datepicker({
       format: "dd mmmm, yyyy",
